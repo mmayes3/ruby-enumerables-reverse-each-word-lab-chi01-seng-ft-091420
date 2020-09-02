89cff004899872168776ba387ae3fa6192280806
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-02 21:02:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(sent)
   my = sent.split
-  
   my.collect do |word|
     word.reverse
   end
-  my.join("")
-  return my 
+ 
 end
 puts reverse_each_word("Hello there, and how are you?")
