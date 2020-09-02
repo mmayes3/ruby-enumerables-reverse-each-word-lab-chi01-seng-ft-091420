c7cddc934d1f8b98d484df134a9e5defb7b181cc
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-02 21:04:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sent)
   my = sent.split
   new_sent = []
   my.collect do |word|
-    word.reverse
+    new_sent.push(word.reverse)
   end
- 
+  p new_sent
 end
-puts reverse_each_word("Hello there, and how are you?")
+reverse_each_word("Hello there, and how are you?")
